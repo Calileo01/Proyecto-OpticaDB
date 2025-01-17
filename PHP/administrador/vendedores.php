28009e6dd4ae612c3d387f5e3e47608395fb49f7
unknown <dr653342@gmail.com>
2025-01-17 14:13:03 -0400

    Arreglos 1

--- PHP/administrador/vendedores.php
+++ PHP/administrador/vendedores.php
@@ -42,7 +42,7 @@
                                     <a href="agregar_vendedor.php"><button class="btn text-white"style="background-color:brown;">Agregar Vendedores</button></a>
                                 </div>
                                 <div class="col">
-                                    Column
+                                    <input type="text" class="form-control" placeholder="Buscar">
                                 </div>
                                 <div class="col">
                                     Column
@@ -85,6 +85,7 @@
         </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 </html>
 <?php
 //session_start();
